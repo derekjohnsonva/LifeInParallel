80cd53d2df9837ecc701f932f479220bb7fa0c90
Derek Johnson <dej3tc@virginia.edu>
2021-03-15 21:10:41 -0400

    First Try at new divideBoard

--- life-parallel.cc
+++ life-parallel.cc
@@ -3,15 +3,20 @@
 #include <vector>
 #include <stdio.h>
 
-struct DataRange
+
+struct Position
 {
-    int cell_start, cell_stop;
+    int x, y;
+};
+struct DataRange 
+{
+    std::vector<Position> indeces;
 };
 struct LifeArgs
 {
     int steps;
     LifeBoard &evenState, &oddState;
-    DataRange dr;
+    std::vector<Position> dr;
     pthread_barrier_t *barrier;
 };
 
@@ -22,16 +27,43 @@ void *RunLife(void *args) {
     int steps = myArgs->steps;
     LifeBoard evenState = myArgs->evenState;
     LifeBoard oddState = myArgs->oddState;
-    DataRange dr = myArgs->dr;
+    std::vector<Position> dr = myArgs->dr;
     pthread_barrier_t *barrier = myArgs->barrier;
 
-    int cs, cf;
-    cs = dr.cell_start;
-    cf = dr.cell_stop;
-    printf("cs=%d, cf=%d, drIndex=%p\n", cs, cf, (void*)&dr);
-
     for(int step = 0; step < steps; ++step) {
-        //wait for all other threads to finish writing.
+        int x, y;
+        bool state_is_even = step %2;
+        LifeBoard *state;
+        LifeBoard *next_state;
+        if(state_is_even) {
+            state = &evenState;
+            next_state = &oddState;
+        } else {
+            state = &oddState;
+            next_state = &evenState;
+        }
+        for(Position p : dr) {
+            int live_in_window = 0;
+            x = p.x;
+            y = p.y;
+            printf("(%d, %d)\n",x,y);
+            /* For each cell, examine a 3x3 "window" of cells around it,
+             * and count the number of live (true) cells in the window. */
+            for (int y_offset = -1; y_offset <= 1; ++y_offset) {
+                for (int x_offset = -1; x_offset <= 1; ++x_offset) {
+                    if (state->at(x + x_offset, y + y_offset)) {
+                        ++live_in_window;
+                    }
+                }
+            }
+            /* Cells with 3 live neighbors remain or become live.
+                Live cells with 2 live neighbors remain live. */
+            next_state->at(x, y) = (
+                live_in_window == 3 /* dead cell with 3 neighbors or live cell with 2 */ ||
+                (live_in_window == 4 && state->at(x, y)) /* live cell with 3 neighbors */
+            );
+
+        }
         pthread_barrier_wait(barrier);
     }
     return NULL;
@@ -39,37 +71,48 @@ void *RunLife(void *args) {
 
 std::vector<DataRange> divideBoard(int threads, LifeBoard &state) {
     int h, w;
-    // get the height and width of the 
-    // values that we will be looking at
     h = state.height() - 2;
     w = state.width() - 2;
     int total_cells = h * w;
     int cells_per_thread = total_cells/threads;
     int cells_left_over = total_cells%threads;
 
-    std::vector<DataRange> drs;
-    // The starting cell should be the cell at position (1,1)
-    int cur_cell = state.width() + 1;
-
-    for(int i = 0; i < threads; i++) {
-        DataRange dr;
-        dr.cell_start = cur_cell;
-        // avoid assigning edge cells to a thread
-        int rows_crossed = total_cells/w;
-        int new_cell = cur_cell + cells_per_thread + (2*rows_crossed);
-        // take care of unevenly divided squares
-        if(cells_left_over > 0) {
-            ++new_cell;
-            --cells_left_over;
-        }
-        dr.cell_stop = new_cell;
-        drs.push_back(dr);
-
-        cur_cell = new_cell + 1;
-
+    int count = 0;
+    int cells_for_cur_thread = cells_per_thread;
+    if(cells_left_over != 0){
+        cells_for_cur_thread++;
+        cells_left_over--;
     }
+    std::vector<DataRange> threadRanges;
+    struct DataRange curRange;
+    for (int y = 1; y < state.height() - 1; ++y) {
+        for (int x = 1; x < state.width() - 1; ++x) {
+            struct Position p = {x, y};
+            // If we need to start making a new thread range
+            if(count >= cells_for_cur_thread) {
+                count = 0;
 
-    return drs;
+                cells_for_cur_thread = cells_per_thread;
+                if(cells_left_over != 0){
+                    cells_for_cur_thread++;
+                    cells_left_over--;
+                }
+                threadRanges.push_back(curRange);
+
+                struct DataRange newRange;
+                curRange = newRange;
+            }
+
+            curRange.indeces.push_back(p);
+
+            count++;
+        }
+    }
+    threadRanges.push_back(curRange);
+    if((int)threadRanges.size() != threads) {
+        printf("threadRange size=%ld, threads=%d", threadRanges.size(), threads);
+    }
+    return threadRanges;
 }
 
 void simulate_life_parallel(int threads, LifeBoard &state, int steps) {
@@ -86,7 +129,7 @@ void simulate_life_parallel(int threads, LifeBoard &state, int steps) {
     std::vector<DataRange> drs = divideBoard(threads, state);
     for(t = 0; t < threads; t++) {
         DataRange dr = drs[t];
-        LifeArgs args = {steps, state, oddState, dr, &barrier};
+        LifeArgs args = {steps, state, oddState, dr.indeces, &barrier};
         pthread_t thread = 0;
         rc = pthread_create(&thread, NULL, RunLife, &args);
         if (rc){
